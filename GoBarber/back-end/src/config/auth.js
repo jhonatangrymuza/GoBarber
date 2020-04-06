ec62a17906676aacd5ef0c98930285fa20a4d283
jhonatan.grymuza <jhonyzanatta@hotmail.com>
2020-04-06 17:53:10 -0300

    Adicionado validacao de formulario autorizacao JWT e verificacao se esta logado

---
+++ GoBarber/back-end/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret:"etasregwefw59fe9w49*f49wq54235",
+    expiresIn: '7d',
+}
--- GoBarber/back-end/src/routes.js
+++ GoBarber/back-end/src/routes.js
@@ -2,17 +2,19 @@ import { Router } from 'express';
 import User from './app/models/User'
 
 import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+
+import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router();
 
-routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store);
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name:"J433ho4nat67an Grymuza",
-        email:"jhon45y567zatta2n32@hotmail.com",
-        password_hash:'123673544256'
-    })
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+
 export default routes;
