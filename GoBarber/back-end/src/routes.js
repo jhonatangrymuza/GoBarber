0dfafa93a33f03ecb12afcb71a330d88dbb5dcfd
jhonatan.grymuza <jhonyzanatta@hotmail.com>
2020-04-13 22:39:57 -0300

    adicionado upload de arquivos

--- GoBarber/back-end/src/routes.js
+++ GoBarber/back-end/src/routes.js
@@ -3,10 +3,14 @@ import User from './app/models/User'
 
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
+import FileController from './app/controllers/FileControlller'
+import multer from 'multer';
+import multerConfig  from './config/multer';
 
 import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router();
+const upload = multer(multerConfig)
 
 routes.post('/sessions', SessionController.store);
 
@@ -16,5 +20,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.post('/files',upload.single('file'),FileController.store)
 
 export default routes;
